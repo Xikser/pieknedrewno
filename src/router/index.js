55eb80aae38a25310754d22f69cb16681ba0a721
Xikser <Xikser@wp.pl>
2021-04-14 12:07:19 +0200

    Update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,21 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-
-  },
-  {
-    path: '/about',
-    name: 'About',
-  }
+	{
+		path: '/',
+		name: 'Strona Główna',
+        component: () => import('../components/Views/Home/Home.vue')
+	},
+	{
+		path: '/about',
+		name: 'O mnie',
+        component: () => import('../components/Views/About/About.vue')
+	}
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+	history: createWebHashHistory(),
+	routes
 })
 
 export default router
