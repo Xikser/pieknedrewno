4d3c501e68366b022d3df973999fa591a2a712a9
Xikser <Xikser@wp.pl>
2021-04-23 13:48:33 +0200

    Create component

--- src/components/Views/Offer/components/Shop/Shop.vue
+++ src/components/Views/Offer/components/Shop/Shop.vue
@@ -1,33 +1,104 @@
-<template functional>
+<template>
 	<section class="shop">
-		<div class="shop__content">
-			<Item
-				v-for="(item, index) in shopitems"
-				:key="index"
-				:item="item"
-			>
-			</Item>
-		</div>
+		<h2 class="category">{{ handleCategory }}</h2>
+			<div class="shop__content" >
+				<Item
+						v-for="(item, index) in getFilteredItems()"
+						:key="index"
+						:item="item"
+						@click="itemView(item)"
+				>
+				</Item>
+			</div>
 	</section>
 </template>
 
 <script>
-import shopitems from '../../Home/components/Shop/ShopItems'
-import Item from "@/components/Views/Offer/components/Shop/Item/Item";
+import Item from "@/components/Views/Offer/components/Shop/Item";
+import ItemView from "@/components/Views/Offer/components/Shop/ItemView/ItemDetails";
+
+import shopItems from "@/components/Views/Offer/components/Shop/libs/ShopItems";
 
 export default {
 	name: 'offer',
-	components: {Item},
+	components: {Item, ItemView},
+	props: {
+		subtype: {
+			type: String,
+		},
+		category: {
+			type: String,
+		},
+		animationName: {
+			type: String,
+		},
+	},
+	watch: {
+		subtype: function () {
+			this.setSubtype(this.subtype)
+		},
+		category: function () {
+			this.setCategory(this.category)
+		},
+	},
 	data() {
 		return {
-			shopitems: shopitems,
+			shopItems: shopItems,
+			selectedSubtype: 'default',
+			view: false,
+			item: Object,
+			handleCategory: 'Wszystko',
+			animation: 'shop',
+		}
+	},
+	methods: {
+		setCategory(category) {
+			this.handleCategory = category
+		},
+
+		setSubtype(subtype) {
+			this.selectedSubtype = subtype
+		},
+
+		getFilteredItems() {
+			if (!this.selectedSubtype || this.selectedSubtype === 'default') {
+				return this.shopItems
+			}
+
+			return this.shopItems.filter(e => e.subtype === this.selectedSubtype)
+		},
+
+		itemView(item) {
+			this.$emit('changeView', item)
 		}
 	},
 }
 </script>
 
 <style lang="sass" scoped>
-@import 'src/assets/sass/variables'
+@import '../../../../../assets/sass/variables'
 
+.category
+	position: relative
+	top: 0
+	left: 8%
+	font-size: 2.3rem
+	text-transform: uppercase
+	cursor: default
 
+.shop
+	position: relative
+	width: 83vw
+	margin-top: 20px
+	margin-left: 50px
+	min-height: 100vh
+
+	&__content
+		padding: 0 70px
+		width: 100%
+		display: flex
+		flex-direction: row
+		flex-wrap: wrap
+		align-items: center
+		justify-content: flex-start
 </style>
